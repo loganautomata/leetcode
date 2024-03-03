5189cfcae060f5cabab5a9a2d96683933b340fd1
Logan Ren <43755964+loganautomata@users.noreply.github.com>
2024-03-03 20:34:09 +0800

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/logan/Documents/Code/leetcode/cpp
-# Build directory: /home/logan/Documents/Code/leetcode/cpp/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
