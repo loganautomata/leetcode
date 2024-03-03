892755a61ca40ed8a17137dd826a27557ce5c8e1
loganautomata <loganautomata@gmail.com>
2024-03-03 20:30:59 +0800

    修改gitignore

--- src/array/easy/1.两数之和/solution.cpp
+++ src/array/easy/1.两数之和/solution.cpp
@@ -10,16 +10,7 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> hashtable;
-        for (int i = 0; i < nums.size(); ++i) {
-            auto it = hashtable.find(target - nums[i]);
-            if (it != hashtable.end()) {
-                return {it->second, i};
-            }
-            hashtable[nums[i]] = i;
-        }
-        return {};      
+         
     }
 };
 // @lc code=end
-
