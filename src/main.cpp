9fbf6e63918b2192cd13f11ea87e24b1a280dcae
loganautomata <loganautomata@gmail.com>
2024-03-19 13:12:37 +0800

    class test

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,51 @@ Input input;
 Output output;
 Solution solution;
 
-int main(int argc, char **argv) {
-    vector<int> vec_arr = {1, 2, 3, 4};
+class Test {
+private:
+    int value_test;
 
-    for (auto i : vec_arr) {
-        cout << i << endl;
+public:
+    Test(int value_test) :
+        value_test(value_test){
+            cout << "value of test is: " << value_test << endl;
+        };
+};
+
+class Base {
+protected:
+    int value;
+
+public:
+    Base(int value) :
+        value(value){};
+};
+
+class DerivedA : public Base {
+private:
+    Test test;
+    int value_a;
+
+public:
+    DerivedA(int value, int value_a, int value_test) :
+        Base(value), value_a(value_a), test(value_test) {
     }
+};
+
+class DerivedB : public Base {
+private:
+    int value_b;
+
+public:
+    DerivedB(int value, int value_b) :
+        Base(value), value_b(value_b) {
+    }
+};
+
+int main(int argc, char **argv) {
+    DerivedA *derived_a = new DerivedA(10, 20, 30);
+
+    delete derived_a;
 
     return 0;
 }
