811d47926499ed6a646d268aa3806f40c543f8e7
loganautomata <loganautomata@gmail.com>
2024-04-02 15:53:12 +0800

    提交camke修改

--- src/main.cpp
+++ src/main.cpp
@@ -8,51 +8,12 @@ Input input;
 Output output;
 Solution solution;
 
-class Test {
-private:
-    int value_test;
-
-public:
-    Test(int value_test) :
-        value_test(value_test){
-            cout << "value of test is: " << value_test << endl;
-        };
-};
-
-class Base {
-protected:
-    int value;
-
-public:
-    Base(int value) :
-        value(value){};
-};
-
-class DerivedA : public Base {
-private:
-    Test test;
-    int value_a;
-
-public:
-    DerivedA(int value, int value_a, int value_test) :
-        Base(value), value_a(value_a), test(value_test) {
-    }
-};
-
-class DerivedB : public Base {
-private:
-    int value_b;
-
-public:
-    DerivedB(int value, int value_b) :
-        Base(value), value_b(value_b) {
-    }
-};
-
 int main(int argc, char **argv) {
-    DerivedA *derived_a = new DerivedA(10, 20, 30);
+    vector<int> arr = {1, 2, 3, 4, 5};
 
-    delete derived_a;
+    for (auto i : arr) {
+        cout << i << endl;
+    }
 
     return 0;
 }
